510efdc10675ea9fc7d6a9a24dd4b6d38f460015
jmasonlee <jmasonlee@gmail.com>
2021-06-05 19:22:36 -0600

    e  Add root enforcement

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    "roots": [
+        "<rootDir>/src"
+    ],
     "testMatch": ["**/__tests__/**/*.[jt]s?(x)","**/?(*.)+(spec|test).[tj]s?(x)"],
     "transform": {
         "^.+\\.(ts|tsx)$": "ts-jest"
